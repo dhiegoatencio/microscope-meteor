7af2325997033560eb39cfe6f4ffe7bb8c763ea1
dhiegoatencio <dhiego.atencio@gmail.com>
2015-01-22 12:39:15 -0200

    first commit

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,22 @@
+var postsData = [
+    {
+        title: 'Introdução ao Telescope',
+        author: 'Sacha',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        author: 'Tom Coleman',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        author: 'Tom Coleman',
+        url: 'http://themeteorbook.com'
+    }
+];
+
+
+Template.postsList.helpers({
+    posts: postsData
+});
